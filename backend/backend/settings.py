b58ff0a91c6ae12439c2855d6bff829c864b900b
unknown <elonmusk0346@gmail.com>
2025-09-30 16:08:30 +0500

    Q

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -154,8 +154,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# Disable collectstatic warnings in API-only deployments
+STATICFILES_DIRS = []
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
